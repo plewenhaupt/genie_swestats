c3a5e035a1d1f9cca770e4b74516c2fb608310b6
Peder Lewenhaupt <plewenhaupt@gmail.com>
2021-01-10 00:26:26 +0100

    Done for today

--- routes.jl
+++ routes.jl
@@ -14,8 +14,8 @@ route("/frontpage", FrontpageController.frontpage)
 using DashboardController
 route("/dashboard", DashboardController.dashboard)
 
-using Gender_populationController
-route("/gender_population", Gender_populationController.gender_population)
+using GenderPopulationController
+route("/gender_population", GenderPopulationController.gender_population)
 
 using PoliticiansController
 route("/politicians", PoliticiansController.politicians)
